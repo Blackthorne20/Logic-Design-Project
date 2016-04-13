b034bd69a68cc50be4454586a8d23861db08df82
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-04-13 14:55:04 -0500

    Register Entity

--- reg.vhd
+++ reg.vhd
@@ -3,16 +3,19 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
  
 entity reg is
-port( a       :  in std_logic_vector(7 downto 0);
-upd     :  in std_logic;
-c       :  out std_logic_vector(7 downto 0));
+port
+(
+	a       :  in std_logic_vector(7 downto 0);
+	upd     :  in std_logic;
+	c       :  out std_logic_vector(7 downto 0)
+);
 end;
  
 architecture r of reg is
 begin
-process(upd) begin
-if rising_edge(upd) then
-c <= a;
-end if;
-end process;
+	process(upd) begin
+		if rising_edge(upd) then
+			c <= a;
+		end if;
+	end process;
 end;
