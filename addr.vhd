91677a63b1edcff5b6ab0a0d3a1e99bf90382032
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-04-13 15:12:57 -0500

    Adder Entity

---
+++ addr.vhd
@@ -0,0 +1,24 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+ 
+entity addr is
+	port
+	(
+		a			: in unsigned	(7 downto 0);
+		b			: in unsigned 	(7 downto 0);
+		en       : in std_logic;
+		result	: out unsigned (7 downto 0)
+	);
+ 
+end entity;
+ 
+architecture rtl of addr is
+begin
+	process(en)
+	begin
+		if(en = '1') then
+		result <= a + b;
+		end if;
+	end process;
+end rtl;
